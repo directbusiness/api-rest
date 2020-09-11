6024cbfff434068a7c9cf568112aafbc73385d87
Ademat Tellecher <ademartellecher@gmail.com>
2020-09-11 11:36:55 -0300

    Feito modificações na estrutura de pastas

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const rotaProdutos = require('../routes/produtos');
+const rotaClientes = require('../routes/clientes');
+const rotaServices = require('../routes/services');
+
+app.use(rotaProdutos);
+app.use(rotaClientes);
+app.use(rotaServices);
+
+module.exports = app;
