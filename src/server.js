243e5c0578c1b3ad9b55902c4e0fb4aa162fdba4
Ademar Tellecher <ademartellecher@gmail.com>
2020-12-03 01:58:43 -0300

    add new dependencies for evolition project-api

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const http = require('http');
+const host = require('ip');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port, () => {
+    console.log(`
+    Server is listen on PORT: ${port}
+    And ip address: ${host.address()}
+    `);
+});
