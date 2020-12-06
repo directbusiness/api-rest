b306bfa55579c6b23fdc8c8f37881afb19874e24
Ademar Tellecher <ademartellecher@gmail.com>
2020-12-06 02:51:49 -0300

    updated seeds, and migrations

--- src/database/migrations/20201203021240_create_users.js
+++ src/database/migrations/20201203021240_create_users.js
@@ -2,8 +2,10 @@ exports.up = function (knex) {
     return knex.schema.createTable('users', (table) => {
         table.increments('id').primary();
         table.text('user_name').unique().notNullable();
+        table.text('user_password').notNullable();
 
         table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
 };
 
