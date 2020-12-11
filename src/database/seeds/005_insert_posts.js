a16b4f9193f62d84ac95e4a78a4ac1e306ac18e9
Ademar Tellecher <ademartellecher@gmail.com>
2020-12-11 05:17:58 -0300

    upgrade in seed and migrations

--- src/database/seeds/005_insert_posts.js
+++ src/database/seeds/005_insert_posts.js
@@ -5,7 +5,7 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('posts').insert([
-        { title: 'post-04', content: 'conteudo do post-01' },
+        { title: 'post-01', content: 'conteudo do post-01' },
         { title: 'post-02', content: 'conteudo do post-02' },
         { title: 'post-03', content: 'conteudo do post-03' },
         { title: 'post-04', content: 'conteudo do post-04' }
