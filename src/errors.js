266ef456cbd49d4842dfe7bc537e2bc6e83a922f
Ademar Tellecher <ademartellecher@gmail.com>
2020-12-15 22:10:01 -0300

    update file on errors functions

--- src/errors.js
+++ src/errors.js
@@ -1,5 +1,8 @@
 module.exports = {
-    error(error, req, res, next) {
+    tryErrors(error, req, res) {
+        const erro = new Error('Rota não encontrada!');
+        erro.status = 404;
+
         res.status(error.status || 500);
         res.json({ error: error.message });
     }
@@ -9,3 +12,26 @@ module.exports = {
 //     res.status(error.status || 500);
 //     res.json({ error: error.message });
 // })
+
+// module.exports = {
+//     tryErrors(req, res) {
+//         const error = new Error("Rota não encontrada!");
+//         error.status = 404;
+
+//         res.status(error.status || 500);
+//         res.send({ message: error.message });
+//     }
+// }
+
+// app.use((req, res, next) => {
+//     const erro = new Error("Rota não encontrada!");
+//     erro.status = 404;
+//     next(erro);
+// });
+
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+//     res.send({
+//         mensagem: error.message
+//     });
+// });
